0d23f490fbd9fdefc054d338b771fcb3b860fe62
avi <avsh555kc@gmail.com>
2024-09-13 01:27:18 +1000

    Nav Bar Part 1

---
+++ about.php
@@ -0,0 +1,6 @@
+<section id="about">
+            <h1>About Us</h1>
+            <p>We are Dance Fusion Studio</p>
+            <p>Dance Studio is where passion meets precision. We provide top-notch dance education in a vibrant and supportive environment. Our mission is to help dancers of all levels improve their technique and enjoy the art of dance.</p>
+            <p>Founded in 2024, Dance Studio has quickly become a hub for dancers in the community. Whether you're a beginner or an experienced dancer, we have something for you.</p>
+</section>
