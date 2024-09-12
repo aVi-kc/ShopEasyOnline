70feed09ef7a37ce1f5fc2576558961dd72a8252
Nishant Nepali <20028766@students.koi.edu.au>
2024-09-12 23:10:19 +1000

    Login and register update

--- login.php
+++ login.php
@@ -1,6 +1,21 @@
-<link rel="stylesheet" href="./assete/login.css">
-<section>
-
-<h1>Login</h1>
-
-</section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/login.css">
+</head>
+<body>
+<div class="login-container">
+        <h2>Login Form</h2>
+        <span id="error-message" class="error"></span>
+        <form name="loginForm" action="process_login.php" method="post" onsubmit="return validateLoginForm()">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Login">
+        </form>
+    </div>
+    <script src="./assets/login.js"></script>
+</body>
+</html>
