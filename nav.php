92db0ce7e0780724e2a3007124058bca38919c33
avi <avsh555kc@gmail.com>
2024-09-12 23:25:36 +1000

    Nav Bar

--- nav.php
+++ nav.php
@@ -1,29 +1,34 @@
+
+<link rel="stylesheet" href="./assets/nav.css">
+
 <nav>
     <div class="logo">
-        <a href="index.php">ShopEasy</a>
+        <img src="./image/logo 1.jpg" alt="ShopEasyOnline">
     </div>
 
     <input type="checkbox" id="menu-toggle">
     <label for="menu-toggle" class="hamburger">&#9776;</label>
 
     <ul class="nav-links">
-        <li><a href="index.php">Home</a></li>
+        <li><a href="./assetes/index.php">Home</a></li>
+        <li><a href="./assetes/product.php">All Products</a></li>
         <li class="dropdown">
-            <a href="#">Products &#9662;</a>
-            <ul class="dropdown-content">
-                <li><a href="#">Seasonal</a></li>
-                <li><a href="#">Promotional</a></li>
-                <li class="dropdown-sub">
-                    <a href="#">Category &#9656;</a>
-                    <ul class="dropdown-sub-content">
-                        <li><a href="#">Jeans</a></li>
-                        <li><a href="#">Shirts</a></li>
-                    </ul>
-                </li>
+    <a href="#">Category &#9662;</a>
+    <ul class="dropdown-content">
+        <li><a href="product.php">All Categories</a></li>
+        <li class="dropdown-sub">
+            <a href="#">Subcategories &#9656;</a>
+            <ul class="dropdown-sub-content">
+                <li><a href="product.php">Jeans</a></li>
+                <li><a href="product.php">Shirts</a></li>
             </ul>
         </li>
-        <li><a href="#">Search</a></li>
-        <li><a href="login.php">Login/Signup</a></li>
+    </ul>
+</li>
+a href="#">Search</a></li>
         <li><a href="cart.php">Cart</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        
     </ul>
 </nav>
